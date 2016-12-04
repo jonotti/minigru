550f43380a38b4d061b4d0fbd6cb9c442cf963d4
jonotti <jonotti97@gmail.com>
2016-12-04 15:24:06 +0000

    Add files via upload

--- chart.js
+++ chart.js
@@ -1,53 +1,17 @@
 var ctx = document.getElementById("myChart").getContext("2d");
-/*var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});*/
-var chartjsData = [];
-for (var i = 0; i < json.length; i++) {
-    chartjsData.push(data[i].);  
-}
 
+/*var test = [];
+$.getJSON("data.json", function (json) {
+    test.push(json.posts[i]);
+
+});*/
 
 var scatterChart = new Chart(ctx, {
     type: 'line',
     data: {
         datasets: [{
             label: 'Hitastig C°',
-            data: chartjsData
-            /*data: [{
+            data: [{
                 x: -10,
                 y: 0
             }, {
@@ -67,7 +31,7 @@ var scatterChart = new Chart(ctx, {
             },{
                 x: 10,
                 y: 0
-            }]*/
+            }]
         }]
     },
     options: {
